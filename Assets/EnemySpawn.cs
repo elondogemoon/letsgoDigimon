ff08dcaeb29053eb5a2162768d6108e1e1f2e8c0
amckdgjs <amckdgjs1@gmail.com>
2024-09-06 01:56:37 +0900

    [feat]적 스폰

--- Assets/EnemySpawn.cs
+++ Assets/EnemySpawn.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    [SerializeField] GameObject enemy;
-    [SerializeField] Enemy _enemy;
-    [SerializeField] Transform[] _spawnPoint;
+    [SerializeField] GameObject enemy; 
+    [SerializeField] Transform[] _spawnPoints;  
+    [SerializeField] float spawnInterval = 2f;  
+    private bool spawning = true;
+
     private void Start()
     {
-        ObjectPoolManager.Instance.CreatePool(enemy, 10);
-        Spawn();
+        ObjectPoolManager.Instance.CreatePool(enemy, 10);  
+        StartCoroutine(SpawnRoutine());  
     }
-    
+
+    // 일정 시간 간격으로 적을 소환하는 코루틴
+    private IEnumerator SpawnRoutine()
+    {
+        while (spawning)
+        {
+            Spawn();  
+            yield return new WaitForSeconds(spawnInterval);  
+        }
+    }
+
+    // 몬스터를 스폰하는 함수
     public void Spawn()
     {
-        ObjectPoolManager.Instance.DequeueObject(enemy);
+        int randomIndex = Random.Range(0, _spawnPoints.Length);  
+        Transform spawnPoint = _spawnPoints[randomIndex];  
+
+        GameObject spawnedEnemy = ObjectPoolManager.Instance.DequeueObject(enemy); 
+        if (spawnedEnemy != null)
+        {
+            spawnedEnemy.transform.position = spawnPoint.position; 
+            spawnedEnemy.SetActive(true);  
+        }
     }
 }
